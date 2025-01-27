4c8fdaf8519b4f647e6e1c14225739159fbb1f43
Haotian Liu <skaum103@outlook.com>
2025-01-27 15:30:39 -0500

    init

---
+++ src/main/java/com/example/form_flow_backend/FormFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.form_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormFlowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/form_flow_backend/FormFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.form_flow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

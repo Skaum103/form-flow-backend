cdd6e1ade508d0345080e15e8b8c03e2326ca1c8
Haotian Liu <skaum103@outlook.com>
2025-02-04 19:02:55 -0500

    Auth, incomplete

--- src/test/java/com/example/form_flow_backend/FormFlowBackendApplicationTests.java
+++ src/test/java/com/example/form_flow_backend/FormFlowBackendApplicationTests.java
@@ -17,19 +17,5 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class FormFlowBackendApplicationTests {
 
-	@BeforeAll
-    static void setUp() {
-		System.setProperty("DEPLOY_MODE", "cloud");
 
-		if (System.getProperty("DEPLOY_MODE").equals("local")) {
-		} else {
-			JSONObject secret = SecretManagerUtil.getSecret(System.getenv("DB_SECRET_NAME"));
-			System.setProperty("DB_USERNAME", secret.getString("username"));
-			System.setProperty("DB_PASSWORD", secret.getString("password"));
-		}
-	}
-
-	@Test
-	void contextLoads() {
-	}
 }

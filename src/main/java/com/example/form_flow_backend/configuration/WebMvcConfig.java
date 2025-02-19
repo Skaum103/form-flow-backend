9eddaab1f8e64b37f90e151f9ddc8b27e9ba2623
Haotian Liu <skaum103@outlook.com>
2025-02-18 20:15:52 -0500

    Cors issue solve

--- src/main/java/com/example/form_flow_backend/configuration/WebMvcConfig.java
+++ src/main/java/com/example/form_flow_backend/configuration/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://from-flow-fe.us-east-1.elasticbeanstalk.com")
+                .allowedOrigins("http://from-flow-fe.us-east-1.elasticbeanstalk.com", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");

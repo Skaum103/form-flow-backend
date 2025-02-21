4e1f2efbcd1ea21ea834daaf956aa6f0f0bb0a91
Haotian Liu <skaum103@outlook.com>
2025-02-20 21:34:19 -0500

    Survey question update endpoint

--- src/main/java/com/example/form_flow_backend/DTO/UpdateQuestionsRequest.java
+++ src/main/java/com/example/form_flow_backend/DTO/UpdateQuestionsRequest.java
@@ -1,23 +1,20 @@
 package com.example.form_flow_backend.DTO;
 
 import com.example.form_flow_backend.model.Question;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class UpdateQuestionsRequest {
+    // Getters and setters
     private String surveyId;
     private List<Question> questions; // Using the entity directly
 
-    // Getters and setters
-    public String getSurveyId() {
-        return surveyId;
-    }
     public void setSurveyId(String surveyId) {
         this.surveyId = surveyId;
     }
-    public List<Question> getQuestions() {
-        return questions;
-    }
+
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
     }

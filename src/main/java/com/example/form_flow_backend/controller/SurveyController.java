e996ec9eb0cf17183f8bbff41006a1901ce9e6d7
Yukun Song <y377song@uwaterlo.ca>
2025-03-04 14:18:19 -0500

    fix GetSurvey bugs

--- src/main/java/com/example/form_flow_backend/controller/SurveyController.java
+++ src/main/java/com/example/form_flow_backend/controller/SurveyController.java
@@ -41,6 +41,7 @@ public class SurveyController {
         System.out.println("just a test");
         System.out.println("just a test");
         System.out.println("just a test");
+        System.out.println("just a test");
         return surveyService.getSurveyDetail(request);
     }
 }
